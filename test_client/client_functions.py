16b08c6531d56a2afe99ce28008a551653cb2cca
Finn Gruwier Larsen <figl@ssi.dk>
2024-02-06 15:20:03 +0100

    Rename function

--- test_client/client_functions.py
+++ test_client/client_functions.py
@@ -2,7 +2,7 @@ import requests
 
 base_url = 'http://bio_api:8001'
 
-def hello_world():
+def call_hello_world():
     url = base_url + '/'
     rest_response = requests.get(url)
     return rest_response
--- test_client/test_basic.py
+++ test_client/test_basic.py
@@ -1,8 +1,8 @@
-from client_functions import hello_world
+from client_functions import call_hello_world
 
 def test_pytest():
     assert 1 == 1
 
 def test_hello_world():
-    result = hello_world()
+    result = call_hello_world()
     assert result.json() == {'message': 'Hello World'}
