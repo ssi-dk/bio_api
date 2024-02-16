b5855dc258119910c307e677b7a1ccab5ef80b28
Finn Gruwier Larsen <figl@dksund.dk>
2024-02-16 22:44:57 +0100

    Add profile2mongo.py

---
+++ profile2mongo.py
@@ -0,0 +1,10 @@
+from os import getenv
+from pandas import read_csv
+
+import mongo
+
+connection_string = getenv('BIO_API MONGO_CONNECTION', 'mongodb://mongodb:27017/bio_api_test')
+print(f"Connection string: {connection_string}")
+mongo_api = mongo.MongoAPI(connection_string)
+
+df = read_csv('test_input/example.tsv', sep='\t')
