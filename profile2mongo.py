814e49194a3302d6b9075fc7b5f931d4db734364
Finn Gruwier Larsen <figl@dksund.dk>
2024-02-17 08:49:09 +0100

    Leave out 'second'

--- profile2mongo.py
+++ profile2mongo.py
@@ -8,7 +8,7 @@ print(f"Connection string: {connection_string}")
 #mongo_api = mongo.MongoAPI(connection_string)
 
 df = read_csv('test_input/example.tsv', sep='\t')
-mongo_doc = df[['Main Key', 'Second']].assign(
-    Aggregated_Data=df.set_index(['Main Key', 'Second']).to_dict(orient='records')
+mongo_doc = df[['Main Key']].assign(
+    Aggregated_Data=df.set_index(['Main Key']).to_dict(orient='records')
     )
 print(mongo_doc)
