1f96be4277ca06b5de397844f5ea2f39bd6a142d
Finn Gruwier Larsen <figl@dksund.dk>
2024-02-17 08:38:09 +0100

    Leave out mongo for now

--- profile2mongo.py
+++ profile2mongo.py
@@ -5,6 +5,6 @@ import mongo
 
 connection_string = getenv('BIO_API MONGO_CONNECTION', 'mongodb://mongodb:27017/bio_api_test')
 print(f"Connection string: {connection_string}")
-mongo_api = mongo.MongoAPI(connection_string)
+#mongo_api = mongo.MongoAPI(connection_string)
 
 df = read_csv('test_input/example.tsv', sep='\t')
