d2996f6030711538a56a551c6fbfbf7084d5faf6
Finn Gruwier Larsen <figl@dksund.dk>
2024-02-17 09:23:56 +0100

    update example.tsv

--- profile2mongo.py
+++ profile2mongo.py
@@ -8,8 +8,8 @@ print(f"Connection string: {connection_string}")
 #mongo_api = mongo.MongoAPI(connection_string)
 
 df = read_csv('test_input/example.tsv', sep='\t')
-new_df = df[['Main Key']].assign(
-    Aggregated_Data=df.set_index(['Main Key']).to_dict(orient='records')
+new_df = df[['ID']].assign(
+    Aggregated_Data=df.set_index(['ID']).to_dict(orient='records')
     )
 print(new_df)
 print(type(new_df))
