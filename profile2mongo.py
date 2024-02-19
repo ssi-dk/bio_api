b92a5e6e7d7e5831d70ff66dfbddae61e6d1337b
Finn Gruwier Larsen <figl@ssi.dk>
2024-02-19 10:25:55 +0100

    Rename var

--- profile2mongo.py
+++ profile2mongo.py
@@ -8,11 +8,10 @@ print(f"Connection string: {connection_string}")
 #mongo_api = mongo.MongoAPI(connection_string)
 
 df = read_csv('test_input/example.tsv', sep='\t')
-new_df = df[['ID']].assign(
+df = df[['ID']].assign(
     profile=df.set_index(['ID']).to_dict(orient='records')
     )
-# print(new_df)
-# print(type(new_df))
-for index, row in new_df.iterrows():
+
+for index, row in df.iterrows():
     # Each rows' to_dict() will be a MongoDB document
     print(row.to_dict())
