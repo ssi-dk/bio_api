3794bff9a1c115482b3a51d27f9de3922f4d952c
Finn Gruwier Larsen <figl@ssi.dk>
2024-02-21 09:44:35 +0100

    Test dmx with fewer sequences

--- test_client/test_basic.py
+++ test_client/test_basic.py
@@ -20,6 +20,6 @@ def test_dmx_from_request():
 
 def test_dmx_from_local_file():
     result = client_functions.call_dmx_from_local_file(
-        file_path = 'test_input/BN_alleles_export.tsv'
+        file_path = 'test_input/BN_alleles_export_5.tsv'
     )
     assert result.status_code == 200
