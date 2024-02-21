00ef6fd5f56667517a00da3006f22fe17e4f485e
Finn Gruwier Larsen <figl@ssi.dk>
2024-02-21 12:42:40 +0100

    First test with MongoDB

--- test_client/test_mongodb.py
+++ test_client/test_mongodb.py
@@ -3,11 +3,9 @@ import pytest
 import client_functions
 from profile2mongo import profile2mongo
 
-@pytest.fixture
-def mongo_ids():
-    return profile2mongo('input_data/BN_alleles_export_5')
+mongo_ids = profile2mongo('input_data/BN_alleles_export_5.tsv')
 
-def xxx_dmx_from_request():
+def test_dmx_from_mongodb():
     result = client_functions.call_dmx_from_mongodb(
     collection='samples',
     seqid_field_path='ID',
