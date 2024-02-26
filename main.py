b37c44b1a31af8a5939d3b28f2088452d1980d82
Finn Gruwier Larsen <figl@ssi.dk>
2024-02-26 15:59:13 +0100

    Add a comment

--- main.py
+++ main.py
@@ -33,6 +33,12 @@ class ProcessingRequest(BaseModel):
         self.id = uuid.uuid4()
 
 class DMXFromMongoDBRequest(BaseModel):
+    """
+    collection: collection to find sequences in
+    seqid_field_path: field path in dotted notation which contains the 'sequence id' the user wants to see
+    profile_field_path: field path in dotted notation which contains the cgMLST allele profiles
+    mongo_ids: the  _id strings for the desired sequence documents
+    """
     collection: str
     seqid_field_path: str
     profile_field_path: str
