5cb2aa19e7caf0653f212ff7f588697aa2cf01f8
Finn Gruwier Larsen <figl@ssi.dk>
2024-06-11 14:33:26 +0200

    Remove spaces

--- test_client/print_column_names.py
+++ test_client/print_column_names.py
@@ -1,4 +1,5 @@
 with open('input_data/Campy2019_metadata_20240530.csv', encoding="ISO-8859-1") as f:
     names = f.readline().split(";")
 for name in names:
+    name = name.replace(" ", "")
     print(name)
