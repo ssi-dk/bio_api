df52ddcb74ddaa597da833f0ee310274fe42f530
Finn Gruwier Larsen <figl@ssi.dk>
2024-06-12 09:32:04 +0200

    Add filename argument

--- test_client/ssv2json.py
+++ test_client/ssv2json.py
@@ -1,4 +1,13 @@
-with open('input_data/Campy2019_metadata_20240530.csv', encoding="ISO-8859-1") as f:
+import argparse
+import pathlib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", help="Semiconon-separated file with BN column names in the first line")
+args = parser.parse_args()
+
+input_file = pathlib.Path(args.input_file)
+
+with open(input_file, encoding="ISO-8859-1") as f:
     bn_colnames = f.readline().strip().split(";")
 
 mapping = dict()
