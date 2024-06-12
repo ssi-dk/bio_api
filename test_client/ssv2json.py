1e0f22269213cb82beb0c0eb2e728ced59fb1965
Finn Gruwier Larsen <figl@ssi.dk>
2024-06-12 09:20:41 +0200

    Create a dict with the column names

--- test_client/ssv2json.py
+++ test_client/ssv2json.py
@@ -1,6 +1,8 @@
 with open('input_data/Campy2019_metadata_20240530.csv', encoding="ISO-8859-1") as f:
-    names = f.readline().split(";")
-for name in names:
-    name = name.replace(" ", "")
-    name = name.replace(",", "_")
-    print(name)
+    bn_colnames = f.readline().strip().split(";")
+
+mapping = dict()
+for colname in bn_colnames:
+    mapping[colname] = ''
+
+print(mapping)
