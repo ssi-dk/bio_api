0a2dc57a1bba00d5b24ba033bddf6a5f808373eb
Finn Gruwier Larsen <figl@ssi.dk>
2024-06-12 11:56:47 +0200

    Update script

--- test_client/ssv2json.py
+++ test_client/ssv2json.py
@@ -4,19 +4,24 @@ from os.path import splitext
 from json import dump
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input_file", help="Semiconon-separated file with BN column names in the first line")
+parser.add_argument("input_file", help="Semiconon-separated file containing mapping beteen BN column names and SOFi column names")
 args = parser.parse_args()
 
 input_file = Path(args.input_file)
 output_filename: str = splitext(args.input_file)[0] + '.json'
 print(f"Output filename: {output_filename}")
 
-with open(input_file, encoding="ISO-8859-1") as i:
-    bn_colnames = i.readline().strip().split(";")
-
 mapping = dict()
-for colname in bn_colnames:
-    mapping[colname] = ''
+
+with open(input_file, encoding="ISO-8859-1") as i:
+    while True:
+        try:
+            conversion_entry = i.readline().strip().split(";")
+            mapping[conversion_entry[0]] = conversion_entry[1].lower()
+        except IndexError:
+            break
+
+print(mapping)
 
 with open(Path(output_filename), 'w') as o:
     dump(mapping, o, indent=4, ensure_ascii=False)
