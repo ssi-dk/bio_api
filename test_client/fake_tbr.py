34697c2efe73c77fbf10918ebc3bc5386273a0c7
Finn Gruwier Larsen <figl@ssi.dk>
2024-08-07 14:40:07 +0200

    Examples

--- test_client/fake_tbr.py
+++ test_client/fake_tbr.py
@@ -40,23 +40,40 @@ if __name__ == '__main__':
     for i_index, i_row in input_data.iterrows():
         output_data.loc[i_index] = (
             i_row.get('Isolatnr'),
-            'provdato',
-            'RunID',
-            'cprnr',
-            'kon',
-            'navn',
-            'alder',
-            'PrimaryIsolate',
-            'Rejse',
-            'landnavn',
-            'KMAdato',
-            'kmanavn',
-            'FUDNR',
-            'ClusterID',
-            'Dato_Epi',
-            'Regionsnavn',
-            'Species',
-            'ST'
+            # provdato example: '2015-01-14 00:00:00'
+            '2015-01-14 00:00:00',
+            # RunID example: 'N_WGS_999'
+            'N_WGS_999',
+            # cprnr example: '2512489996'
+            '2512489996',
+            # kon example: 'K'
+            'K',
+            # navn example: 'BERGGREN, NANCY ANN'
+            'BERGGREN, NANCY ANN',
+            # alder example: 72
+            72,
+            # PrimaryIsolate example: 1
+            1,
+            # Rejse example: 'Ja'
+            'Ja',
+            # landnavn example: 'IBIZA, MALLORCA'
+            'IBIZA, MALLORCA',
+            # KMAdato example: '2015-01-20 00:00:00'
+            '2015-01-20 00:00:00',
+            # kmanavn example: 'KMA RegionSjælland'
+            'KMA RegionSjælland',
+            # FUDNR example: '2211'
+            '2211',
+            # ClusterID example: 'ST99#99'
+            'ST99#99',
+            # Dato_Epi example: '2024-03-22 18:59:01.917'
+            '2024-03-22 18:59:01.917',
+            # Regionsnavn example: 'SJÆLLAND'
+            'SJÆLLAND',
+            # Species example: 'Escherichia coli'
+            'Escherichia coli',
+            # ST example: 583
+            583
         )
         print(output_data)
     
