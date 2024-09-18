3001b3be3cceb541c3037d8452740e76bfbd9c65
Finn Gruwier Larsen <figl@ssi.dk>
2024-09-18 13:54:27 +0200

    Update new_project_from_urls

--- test_client/new_project_from_urls.py
+++ test_client/new_project_from_urls.py
@@ -6,9 +6,8 @@ from json import dumps
 from microreact_integration import common
 from microreact_integration.functions import new_project
 
-parser = argparse.ArgumentParser(description="Create a new minimal project in Microreact using a tree and a metadata table from files.")
-parser.add_argument("tree", help="Path to a Newick file containing the initial tree")
-parser.add_argument("metadata", help="Path to a metadata file")
+parser = argparse.ArgumentParser(description="Create a new minimal project in Microreact using an external url to a metadata table.")
+parser.add_argument("metadata_url", help="URL to a metadata file")
 parser.add_argument(
     "--project_name",
     help="Project name (can be changed later in web interface)",
@@ -21,28 +20,11 @@ parser.add_argument(
     )
 args = parser.parse_args()
 
-with open(Path(args.tree), 'r') as tree_file:
-    newick = tree_file.read()
-
-with open(Path(args.metadata), 'r') as metadata_file:
-    header_line=True
-    metadata_values = list()
-    for line in metadata_file:
-        if header_line:
-            metadata_keys = line.strip().split('\t')
-            print("Metadata columns:")
-            print(metadata_keys)
-            header_line = False
-        else:
-            metadata_values.append(line.strip().split('\t'))
-
 print(f"Name of created project will be {args.project_name}")
 
 rest_response = new_project(
     project_name=args.project_name,
-    tree_calcs=[{'method': 'single', 'result': newick}],
-    metadata_keys=metadata_keys,
-    metadata_values=metadata_values,
+    metadata_url = args.metadata_url,
     mr_access_token=common.MICROREACT_ACCESS_TOKEN,
     mr_base_url=common.MICROREACT_BASE_URL,
     verify = not args.noverify
