28fc338fad0dd60c96cf4d440b3a73a7638ab162
Finn Gruwier Larsen <figl@ssi.dk>
2024-10-09 14:20:54 +0200

    Add queue_name as command line arg

--- consume_hpc_response.py
+++ consume_hpc_response.py
@@ -1,6 +1,15 @@
 import asyncio
 import sys
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--queue_name",
+    default='hpc_response',
+    help="RabbitMQ queue to consume. Here you can enter 'hpc_call' to monitor calls. Default is 'hpc_response' which will monitor responses."
+)
+args = parser.parse_args()
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -12,7 +21,7 @@ async def main() -> None:
     host = "amqp://guest:guest@rabbitmq/"
     listener = sofi_messenger.SOFIMessenger(host)
 
-    hpc_response_iter = listener.consume()
+    hpc_response_iter = listener.consume(args.queue_name)
     async for hpc_response in hpc_response_iter:
         print(f"Received HPC response: {hpc_response}")
 
