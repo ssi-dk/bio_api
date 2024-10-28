9d86ea059e661bf1e090df00aa074d20c5af452b
Finn Gruwier Larsen <figl@ssi.dk>
2024-10-28 15:18:06 +0100

    Add an arbitrary filename

--- test_init_snp_calculation.py
+++ test_init_snp_calculation.py
@@ -16,7 +16,7 @@ async def main() -> None:
     snp_calc = SNPCalculation(
         seq_collection='campy_2019',
         seqid_field_path='categories.sample_info.summary.sofi_sequence_id',
-        filename_field_path=''
+        filename_field_path='field.path.to.fasta.file',
         seq_mongo_ids=['666febab9871ba945a1a11f0', '666febac9871ba945a1a11f1', '666febad9871ba945a1a11f2'],
         reference_mongo_id='666febad9871ba945a1a11f3',
         depth='7',
