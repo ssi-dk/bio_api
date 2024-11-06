79398401950a4084116bc0d6e8808e2ce36a6c27
Finn Gruwier Larsen <figl@ssi.dk>
2024-11-06 15:32:32 +0100

    It wørkzz

--- test_pymongo_connection.py
+++ test_pymongo_connection.py
@@ -2,9 +2,11 @@ from os import getenv
 
 import pymongo
 
-connection_string = getenv('BIO_API_MONGO_CONNECTION', 'mongodb://mongodb:27017/bio_api_test')
-print(connection_string)
-connection = pymongo.MongoClient(connection_string)
-db = connection.get_database()
-sample = db.samples.find_one()
-print(sample)
+client = pymongo.MongoClient(
+    username="admin",
+    password=input("Mongo password: "),
+    directConnection=True
+)
+db = client['sofi-dev']
+collections = db.list_collections()
+print(list(collections))
