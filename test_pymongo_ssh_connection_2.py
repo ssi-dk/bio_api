525f28dd8cb70946481aa3e317fcec5ef1358127
Finn Gruwier Larsen <figl@ssi.dk>
2024-11-06 11:32:45 +0100

    Update

--- test_pymongo_ssh_connection_2.py
+++ test_pymongo_ssh_connection_2.py
@@ -1,42 +1,23 @@
 from os import getenv
 
-from pymongo import MongoClient
-from sshtunnel import SSHTunnelForwarder
+import paramiko
+import sshtunnel
 
-ssh_host = 'sshhost'
-ssh_port = 22
-ssh_user = 'sshuser'
-ssh_private_key_path = 'privatekey'
-mongo_host = 'mongodbserver'
-mongo_port = 27017
-mongo_user = 'user'
-mongo_password = 'password'
-mongo_db = 'db'
-collection_name = 'collection_name'
-
-
-try:
-   with open(ssh_private_key_path, 'r') as f:
-       print("Private key file is accessible.")
-except FileNotFoundError:
-   raise FileNotFoundError(f"Private key file not found at {ssh_private_key_path}")
-except PermissionError:
-   raise PermissionError(f"Permission denied for private key file at {ssh_private_key_path}")
-
-
-with SSHTunnelForwarder(
-   (ssh_host, ssh_port),
-   ssh_username=ssh_user,
-   ssh_pkey=ssh_private_key_path,
-   remote_bind_address=(mongo_host, mongo_port)
+with sshtunnel.open_tunnel(
+    ('10.32.244.37', 22),
+    ssh_username="fingru",
+    ssh_password=(input("SSH password: ")),
+    remote_bind_address=('10.45.129.11', 27017),
+    local_bind_address=('0.0.0.0', 27017)
 ) as tunnel:
-    client = MongoClient(f'mongodb://{mongo_user}:{mongo_password}@127.0.0.1:{tunnel.local_bind_port}/{mongo_db}?compressors=disabled&gssapiServiceName=mongodb')
-
-
-    db = client[mongo_db]
-    collection = db[collection_name]
-    document = collection.find_one()
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect('127.0.0.1', 27017)
+    # do some operations with client session
     client.close()
+
+print('FINISH!')
 
 
 # connection_string = getenv('BIO_API_MONGO_CONNECTION', 'mongodb://mongodb:27017/bio_api_test')
