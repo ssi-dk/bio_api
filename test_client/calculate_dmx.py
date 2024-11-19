2cd99d2d2b9f1fcb022f4a467e1158616391a206
Finn Gruwier Larsen <figl@ssi.dk>
2024-11-19 17:08:06 +0100

    Make calculate_dmx.py

--- test_client/calculate_dmx.py
+++ test_client/calculate_dmx.py
@@ -1,6 +1,7 @@
 from os import getenv
 import argparse
 import pymongo
+from bson import ObjectId
 
 import client_functions
 
@@ -10,11 +11,11 @@ if __name__ == '__main__':
                     description="Calculate a random distance matrix for testing."
     )
 
-    parser.add_argument('--sequence_count', type=int, default=10)
+    parser.add_argument('mongo_ids', type=str, help="ObjecIDs as strings separated by commas")
     parser.add_argument('--seq_collection', type=str, help="name of collection that contains sequence documents", default='samples')
-    parser.add_argument('--seqid_field_path', type=str, help="path for seqid field - dots are translated into nested fields", default='name')
-    parser.add_argument('--profile_field_path', type=str, help="path for profile field - dots are translated into nested fields", default='profile')
-    parser.add_argument('--species_field_path', type=str, help="path for species field - dots are translated into nested fields", default='species')
+    parser.add_argument('--seqid_field_path', type=str, help="path for seqid field - dots are translated into nested fields", default='categories.sample_info.summary.sofi_sequence_id')
+    parser.add_argument('--profile_field_path', type=str, help="path for profile field - dots are translated into nested fields", default='categories.cgmlst.report.alleles')
+    parser.add_argument('--species_field_path', type=str, help="path for species field - dots are translated into nested fields", default='categories.species_detection.summary.species ')
     parser.add_argument('--species', type=str, help="Species to search for", default='Salmonella enterica')
     args = parser.parse_args()
     connection_string = getenv('BIO_API MONGO_CONNECTION', 'mongodb://mongodb:27017/bio_api_test')
@@ -25,22 +26,20 @@ if __name__ == '__main__':
     print(f"MongoDB field containing 'user' sequence id: {args.seqid_field_path}")
     print(f"MongoDB field containing cgMLST profile: {args.profile_field_path}")
 
-    # Find ids based on species and the desired sequence count
-    pipeline = [
-        { '$sample': { 'size': args.sequence_count } },
-        { '$project': { '_id': 1 } }
-    ]
-    mongo_cursor = db[args.seq_collection].aggregate(pipeline)
-    mongo_ids = [ str(s['_id']) for s in mongo_cursor ]
-    print("Mongo IDs:")
-    print(mongo_ids)
+    mongo_str_all: str = args.mongo_ids
+    string_list: str = mongo_str_all.split(',')
+    print(f"Mongo IDs to search for: {string_list}")
+    object_ids = [ObjectId(s) for s in string_list]
+
+    # Make sure we can actually find those documents
+    assert db[args.seq_collection].count_documents({'_id': {'$in': object_ids}}) == len(string_list)
 
     # Initialize the calculation
     response = client_functions.call_dmx_from_mongodb(
         seq_collection=args.seq_collection,
         seqid_field_path=args.seqid_field_path,
         profile_field_path=args.profile_field_path,
-        mongo_ids=mongo_ids)
+        mongo_ids=string_list)
     print("Response:")
     print(response)
     print("Response as JSON:")
